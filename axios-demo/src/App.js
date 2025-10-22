ea7ec02520ef7f0d3d275772690dca7757dfa726
Trinh Huy Long <trinhhuylong2005@gmail.com>
2025-10-22 11:53:03 +0700

    upload

--- axios-demo/src/App.js
+++ axios-demo/src/App.js
@@ -5,48 +5,50 @@ import ClassPage from "./components/ClassPage";
 import axios from "axios";
 import { useEffect } from "react";
 
-function App() {
-  const [currentPage, setCurrentPage] = useState("home");
-  const [selectedClass, setSelectedClass] = useState(null);
-  const [classData, setClassData] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:9999/Class")
-      .then((response) => {
-        setClassData(response.data);
-        console.log("Fetched class data:", response.data);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  const handleClassClick = (classCode) => {
-    setSelectedClass(classCode);
-    setCurrentPage("class");
-  };
-
-  const handleBackToHome = () => {
-    setCurrentPage("home");
-    setSelectedClass(null);
-  };
-
-  if (!classData) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="App">
-      {currentPage === "home" ? (
-        <HomePage classes={classData} onClassClick={handleClassClick} />
-      ) : (
-        <ClassPage
-          classData={classData.find((c) => c.ClassCode === selectedClass)}
-          allData={classData}
-          onBack={handleBackToHome}
-        />
-      )}
-    </div>
-  );
-}
+function App() {}
 
 export default App;
+
+// function App2() {
+//   const [currentPage, setCurrentPage] = useState("home");
+//   const [selectedClass, setSelectedClass] = useState(null);
+//   const [classData, setClassData] = useState(null);
+
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:9999/Class")
+//       .then((response) => {
+//         setClassData(response.data);
+//         console.log("Fetched class data:", response.data);
+//       })
+//       .catch((error) => console.error("Error fetching data:", error));
+//   }, []);
+
+//   const handleClassClick = (classCode) => {
+//     setSelectedClass(classCode);
+//     setCurrentPage("class");
+//   };
+
+//   const handleBackToHome = () => {
+//     setCurrentPage("home");
+//     setSelectedClass(null);
+//   };
+
+//   if (!classData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       {currentPage === "home" ? (
+//         <HomePage classes={classData} onClassClick={handleClassClick} />
+//       ) : (
+//         <ClassPage
+//           classData={classData.find((c) => c.ClassCode === selectedClass)}
+//           allData={classData}
+//           onBack={handleBackToHome}
+//         />
+//       )}
+//     </div>
+//   );
+// }
